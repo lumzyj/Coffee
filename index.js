d57111c917b59eabd6756351b0a69bd0b7c5b201
olumide oni <mide@olumides-MacBook-Air.local>
2022-10-01 19:40:46 +0100

    your message

---
+++ index.js
@@ -0,0 +1,104 @@
+const prompt = require("prompt-sync")();
+
+//Create your function below this line.
+//The first parameter should be the weight and the second should be the height.
+
+// function bmiCalculator(weight,height){
+//         bmi = Math.floor(weight/(height*height));
+//         console.log(bmi);
+//         return bmi;
+ 
+       
+// }
+
+// var bmi = bmiCalculator(65, 1.8); 
+
+
+
+
+// function loveCalculator(){
+//   var love = prompt("Please enter your name");
+//   var crush = prompt("Please enter your crush's name");
+
+//   var calculate = Math.floor(Math.random() * 100 +1);
+//   console.log("dear"+ love + "and" + crush + "your match percentage is" + calculate);
+
+//   return calculate;
+// }
+
+// loveCalculator()
+
+  
+  
+  
+  // var guestlist = ["Angela","Markdown","tolu", "james", "joseph", "lumzy"] 
+
+  // var guest = prompt("Enter your name");
+
+  // if ( guest = guestlist.includes(guest)) {
+  //    console.log("Welcome");
+  // }
+  // else{
+  //   console.log("maybe next time");
+  // }
+  
+// var output = [];
+// var count = 1
+
+// function fizzBuzz() {
+//   output.push(count);
+//   count++;
+//   console.log(output);
+//   if (count % 3 === 0){
+//       output.push("Fizz");
+//   }
+//   else if(count % 5 === 0){
+//       output.push("Buzz");
+//   }
+//   else if(count % 3 === 0 && count % 5 === 0){
+//     output.push("FizzBuzz");
+// }else{
+//   output.push("count")
+// }
+
+// }
+
+// fizzBuzz();
+
+
+
+
+// function whosPaying(names) {
+    
+  /******Don't change the code above*******/
+      
+      //Write your code here.
+      //var names = [];
+      //var num = names.length;
+      // var random = Math.floor((Math.random()*names.length));
+      
+      // return names[random] + " is going to buy lunch today!";
+      
+  
+  /******Don't change the code below*******/    
+  // }
+  
+  // console.log(whosPaying(["Angela","Ben","Jenny","Michael","Chloe"]));
+  
+  function bottles(){
+
+    var beer = 99;
+    beerdrop = beer - 1;
+    while (beer <  99)
+
+   
+      
+
+      console.log(beer + " bootle of beer on the wall," + beer + " bootle of water, take on down and pass it around ," +  beerdrop + "bottle of beer" );
+      
+      beer = beer -1;
+      
+    }
+
+  return bottles();
+
